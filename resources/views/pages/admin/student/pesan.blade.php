0056469b8f3fc17ec97a57e7a9231fd5607c018d
santa2802 <104416740+santa2802@users.noreply.github.com>
2022-05-01 23:33:33 +0700

    createPesan
    create file form pesan

---
+++ resources/views/pages/admin/student/pesan.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form Pesan</title>
+</head>
+<style>
+    h3{
+        padding-top: 70px;
+    }
+</style>
+<body>
+    <center>
+        <h3>Pesan</h3>
+        <textarea name="komentar" cols="100" rows="20" placeholder="Isi pesan anda"></textarea>
+        <br><br><input type="submit" value="Kirim">
+    </center>
+</body>
+</html>
